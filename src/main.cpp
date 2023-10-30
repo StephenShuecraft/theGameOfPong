1cbf8f7fdb80622e85767e179ca50047332894b7
EricWRogers <ericwilliamrogers@gmail.com>
2023-10-30 10:39:07 -0500

    Update Canis!

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,53 @@
 #include <windows.h>
 #endif
 
-#include <iostream>
+#include <Canis/App.hpp>
+#include <Canis/ECS/Decode.hpp>
+#include <Canis/Canis.hpp>
+#include <Canis/SceneManager.hpp>
+#include <Canis/ECS/Systems/RenderHUDSystem.hpp>
+#include <Canis/ECS/Systems/RenderTextSystem.hpp>
+#include <Canis/ECS/Systems/SpriteRenderer2DSystem.hpp>
+#include <Canis/ECS/Systems/SpriteAnimationSystem.hpp>
+#include <Canis/ECS/Systems/CollisionSystem2D.hpp>
+#include <Canis/ECS/Systems/ButtonSystem.hpp>
 
-#include "App.hpp"
+#include "ECS/ScriptableEntities/DebugCamera2D.hpp"
+#include "ECS/ScriptableEntities/BeachBall.hpp"
 
 int main(int argc, char* argv[])
 {
-    App app;
+    Canis::App app;
 
-    app.Run();
+    // decode system
+    app.AddDecodeSystem(Canis::DecodeButtonSystem);
+    app.AddDecodeSystem(Canis::DecodeCollisionSystem2D);
+	app.AddDecodeSystem(Canis::DecodeSpriteAnimationSystem);
+
+    // decode render system
+    app.AddDecodeRenderSystem(Canis::DecodeRenderHUDSystem);
+    app.AddDecodeRenderSystem(Canis::DecodeRenderTextSystem);
+    app.AddDecodeRenderSystem(Canis::DecodeSpriteRenderer2DSystem);
+
+    // decode scriptable entities
+    app.AddDecodeScriptableEntity(DecodeDebugCamera2D);
+    app.AddDecodeScriptableEntity(DecodeBeachBall);
+
+    // decode component
+    app.AddDecodeComponent(Canis::DecodeTagComponent);
+    app.AddDecodeComponent(Canis::DecodeCamera2DComponent);
+    app.AddDecodeComponent(Canis::DecodeRectTransformComponent);
+    app.AddDecodeComponent(Canis::DecodeColorComponent);
+    app.AddDecodeComponent(Canis::DecodeTextComponent);
+    app.AddDecodeComponent(Canis::DecodeSprite2DComponent);
+    app.AddDecodeComponent(Canis::DecodeUIImageComponent);
+    app.AddDecodeComponent(Canis::DecodeUISliderComponent);
+    app.AddDecodeComponent(Canis::DecodeSpriteAnimationComponent);
+    app.AddDecodeComponent(Canis::DecodeCircleColliderComponent);
+
+    app.AddScene(new Canis::Scene("sprite_demo", "assets/scenes/sprite_demo.scene"));
+
+    app.Run("sprite_demo");
 
     return 0;
 }
