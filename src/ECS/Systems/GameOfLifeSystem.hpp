e1f10b35f9ed7e6875b15d20e4569405575c6a1a
EricWRogers <ericwilliamrogers@gmail.com>
2023-11-26 15:46:32 -0600

    Update Final Game Of Life

--- src/ECS/Systems/GameOfLifeSystem.hpp
+++ src/ECS/Systems/GameOfLifeSystem.hpp
@@ -55,13 +55,49 @@ public:
         for(auto[entity, rectTransform, color, gameOfLife] : view.each())
         {
             // turn on cell
-            
+            if (GetInputManager().GetLeftClick())
+            {
+                if (GetInputManager().mouse.x > rectTransform.position.x &&
+                    GetInputManager().mouse.x < rectTransform.position.x + rectTransform.size.x)
+                {
+                    if (GetInputManager().mouse.y > rectTransform.position.y &&
+                        GetInputManager().mouse.y < rectTransform.position.y + rectTransform.size.y)
+                    {
+                        gameOfLife.currentState = true;
+                    }
+                }
+            }
 
             // turn off cell
-            
+            if (GetInputManager().GetRightClick())
+            {
+                if (GetInputManager().mouse.x > rectTransform.position.x &&
+                    GetInputManager().mouse.x < rectTransform.position.x + rectTransform.size.x)
+                {
+                    if (GetInputManager().mouse.y > rectTransform.position.y &&
+                        GetInputManager().mouse.y < rectTransform.position.y + rectTransform.size.y)
+                    {
+                        gameOfLife.currentState = false;
+                    }
+                }
+            }
         }
 
         // toggel running
+        if (GetInputManager().JustPressedKey(SDLK_SPACE))
+        {
+            m_runRulesUpdate = !m_runRulesUpdate;
+
+            Canis::Entity titleText = GetScene().FindEntityWithTag("TITLE");
+
+            Canis::RectTransformComponent& rect = titleText.GetComponent<Canis::RectTransformComponent>();
+            Canis::TextComponent& text = titleText.GetComponent<Canis::TextComponent>();
+
+            if (m_runRulesUpdate)
+                Canis::Text::Set(text, rect, "Game of Life Demo | Running");
+            else
+                Canis::Text::Set(text, rect, "Game of Life Demo | Paused");
+        }
 
         // rules loop
         if (m_runRulesUpdate)
@@ -75,18 +111,52 @@ public:
                 for(auto[entity, rectTransform, color, gameOfLife] : view.each())
                 {
                     // find index
+                    int xIndex = (static_cast<int>(rectTransform.position.x + loader->cellSize) / loader->cellSize) - 1;
+                    int yIndex = (static_cast<int>(rectTransform.position.y + loader->cellSize) / loader->cellSize) - 1;
+                    
+                    int count = 0;
+                    int sizeX = loader->cells[0].size();
+                    int sizeY = loader->cells.size();
                     
                     // rap the index around the screen
+                    int right = ((xIndex + 1) >= sizeX) ? 0 : xIndex + 1;
+                    int left = ((xIndex - 1) < 0) ? sizeX - 1 : xIndex - 1;
+                    int up = ((yIndex + 1) >= sizeY) ? 0 : yIndex + 1;
+                    int down = ((yIndex - 1) < 0) ? sizeY - 1 : yIndex - 1;
 
                     // check neighbors
+                    count += (loader->cells[up][right].GetComponent<GameOfLifeComponent>().currentState == true); // top right
+                    count += (loader->cells[yIndex][right].GetComponent<GameOfLifeComponent>().currentState == true); // right
+                    count += (loader->cells[down][right].GetComponent<GameOfLifeComponent>().currentState == true); // bottom right
+                    count += (loader->cells[up][xIndex].GetComponent<GameOfLifeComponent>().currentState == true); // top center
+                    count += (loader->cells[down][xIndex].GetComponent<GameOfLifeComponent>().currentState == true); // bottom center
+                    count += (loader->cells[up][left].GetComponent<GameOfLifeComponent>().currentState == true); // top left
+                    count += (loader->cells[yIndex][left].GetComponent<GameOfLifeComponent>().currentState == true); // left
+                    count += (loader->cells[down][left].GetComponent<GameOfLifeComponent>().currentState == true); // bottom left
 
                     // rule one
+                    if (count < 2 && gameOfLife.currentState == true)
+                    {
+                        gameOfLife.nextState = false;
+                        gameOfLife.update = true;
+                    }
 
                     // rule two
+                    if (count > 3 && gameOfLife.currentState == true)
+                    {
+                        gameOfLife.nextState = false;
+                        gameOfLife.update = true;
+                    }
 
                     // rule three
 
+
                     // rule four
+                    if (count == 3 && gameOfLife.currentState == false)
+                    {
+                        gameOfLife.nextState = true;
+                        gameOfLife.update = true;
+                    }
                 }
             }
         }
